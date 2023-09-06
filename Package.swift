6cb2090f63dceb3b51f92a7bbc6dbdefeb31a3d6
marcprux <mwp1@cornell.edu>
2023-09-06 09:09:48 -0400

    Modernize package

--- Package.swift
+++ Package.swift
@@ -7,28 +7,15 @@ let package = Package(
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
     .library(name: "SkipYAML", targets: ["SkipYAML"]),
-    .library(name: "SkipYAMLKt", targets: ["SkipYAMLKt"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.6.4"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.1.0"),
-        .package(url: "https://source.skip.tools/skip-lib.git", from: "0.2.0"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.14"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.6.39"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.1.9"),
     ],
     targets: [
-    .target(name: "SkipYAML", plugins: [.plugin(name: "skippy", package: "skip")]),
-    .target(name: "SkipYAMLKt", dependencies: [
-        "SkipYAML",
-        .product(name: "SkipUnitKt", package: "skip-unit"),
-        .product(name: "SkipLibKt", package: "skip-lib"),
-        .product(name: "SkipFoundationKt", package: "skip-foundation"),
-    ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+    .target(name: "SkipYAML", dependencies: [.product(name: "SkipFoundation", package: "skip-foundation", condition: .when(platforms: [.macOS]))], plugins: [.plugin(name: "skipstone", package: "skip")]),
     .testTarget(name: "SkipYAMLTests", dependencies: [
         "SkipYAML"
-    ], plugins: [.plugin(name: "skippy", package: "skip")]),
-    .testTarget(name: "SkipYAMLKtTests", dependencies: [
-        "SkipYAMLKt",
-        .product(name: "SkipUnit", package: "skip-unit"),
-    ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+    ], plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
--- Sources/SkipYAMLKt/SkipYAMLBundle.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-public extension Bundle {
-    static let SkipYAMLBundle = Bundle.module
-}
